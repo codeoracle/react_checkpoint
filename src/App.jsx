0c4948d04f87243d08c156d0d6964ae3388215fa
Yunus Mustapha <yunusmustapha2.com>
2023-04-05 10:49:08 +0100

    commit react checkpoint

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import CreateForm from "./form/CreateForm"
+import Menu from "./navbar/Menu"
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Menu/>
+     <CreateForm/>
+    </div>
+  )
+}
+
+export default App
